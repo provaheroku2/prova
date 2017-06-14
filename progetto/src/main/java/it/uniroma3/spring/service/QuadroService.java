8b228fb3416d822492473fe4e107537d9be65e58
Nicolas Zaccaria <nic.zac95@gmail.com>
2017-06-14 13:13:11 +0200

    creazione QuadroService

---
+++ progetto/src/main/java/it/uniroma3/spring/service/QuadroService.java
@@ -0,0 +1,28 @@
+package it.uniroma3.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import it.uniroma3.spring.repository.QuadroRepository;
+import it.uniroma3.spring.model.Customer;
+import it.uniroma3.spring.model.Quadro;
+
+@Service
+public class QuadroService {
+	@Autowired
+    private QuadroRepository quadroRepository; 
+
+    public Iterable<Quadro> findAll() {
+        return this.quadroRepository.findAll();
+    }
+
+    @Transactional
+    public void add(final Quadro quadro) {
+        this.quadroRepository.save(quadro);
+    }
+
+	public Quadro findbyId(Long id) {
+		return this.quadroRepository.findOne(id);
+	}
+
+}
