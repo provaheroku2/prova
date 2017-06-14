98d2f1ad8b62a61a1c1005d090512908bf891eb1
Alessandro Martinelli <alex.3@live.it>
2017-06-14 14:52:09 +0200

    revisione

--- progetto/src/main/java/it/uniroma3/spring/controller/GalleriaController.java
+++ progetto/src/main/java/it/uniroma3/spring/controller/GalleriaController.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import it.uniroma3.spring.model.Customer;
-import it.uniroma3.spring.service.QuadroService;
+
 
 
 @Controller
 public class GalleriaController  {
 	
     @Autowired
-    private QuadroService quadroservice; 
+    
+   
 
     @GetMapping("/galleria")
-    public String showForm(Customer customer) {
+    public String showForm() {
         return "form";
     }
 
