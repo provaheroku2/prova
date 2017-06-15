8f24818c7a216483989c50ac84164fa7d2f84cf6
Alessandro Martinelli <alex.3@live.it>
2017-06-15 18:27:26 +0200

    modifiche sicurezza

--- progetto/src/main/java/it/uniroma3/spring/security/SecurityConfig.java
+++ progetto/src/main/java/it/uniroma3/spring/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().antMatchers("/","/galleriaquadri","/galleriaQuadro","/galleriaautori","/galleriaAutore","/metodiDiVisualizzazione.html").permitAll().antMatchers("/admin").hasRole("ADMIN")
+        http.authorizeRequests().antMatchers("/","/galleriaquadri","/galleriaQuadro","/galleriaautori","/galleriaAutore","/metodiDiVisualizzazione.html","/mostraAnni","/ricercaQuadriAnno").permitAll().antMatchers("/admin").hasRole("ADMIN")
         .anyRequest().authenticated()
         .and()
         .formLogin()
