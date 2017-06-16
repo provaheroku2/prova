7bda2e75e26f7db3099bf8c81c558a54231ff9d1
Nicolas Zaccaria <nic.zac95@gmail.com>
2017-06-16 11:46:12 +0200

    Navigabilita e ricerca autori

--- progetto/src/main/java/it/uniroma3/spring/service/AutoreService.java
+++ progetto/src/main/java/it/uniroma3/spring/service/AutoreService.java
@@ -1,5 +1,7 @@
 package it.uniroma3.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,24 +14,32 @@ import it.uniroma3.spring.repository.AutoreRepository;
 @Service
 public class AutoreService {
 	@Autowired
-    private AutoreRepository autoreRepository; 
+	private AutoreRepository autoreRepository; 
 
-    public Iterable<Autore> findAll() {
-        return this.autoreRepository.findAll();
-    }
+	public Iterable<Autore> findAll() {
+		return this.autoreRepository.findAll();
+	}
 
-    @Transactional
-    public void add(final Autore autore) {
-        this.autoreRepository.save(autore);
-    }
+	@Transactional
+	public void add(final Autore autore) {
+		this.autoreRepository.save(autore);
+	}
 
 	public Autore findbyId(Long id) {
 		return this.autoreRepository.findOne(id);
 	}
-	
+
 	public void elimina(Autore autore){
 		autoreRepository.delete(autore);
 	}
 
+	public List<Autore> fingByCognome(String cognome){
+		return this.autoreRepository.findByCognome(cognome);			
+	}
 	
+	public List<Autore> fingByNazionalita(String nazionalita){
+		return this.autoreRepository.findByNazionalita(nazionalita);			
+	}
+	
+
 }
