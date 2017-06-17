9e56a651492c772c43e53475425135ba412158d4
Nicolas Zaccaria <nic.zac95@gmail.com>
2017-06-17 16:59:29 +0200

    navigabilità da tabella opere a rispettivo autore

--- progetto/src/main/java/it/uniroma3/spring/controller/GalleriaController.java
+++ progetto/src/main/java/it/uniroma3/spring/controller/GalleriaController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.uniroma3.spring.model.Autore;
@@ -35,7 +34,11 @@ public class GalleriaController  {
 		return "/metodiDiVisualizzazione";
 	}
 
-
+	@GetMapping("/ricercaAutoreId")
+	public String ricercaAutoriPerId(@RequestParam("autoreId") Long id, Model model){
+		model.addAttribute("autori", autoreservice.findbyId(id));
+		return "MostraAutori";
+	}
 
 	@PostMapping("/ricercaAutoriCognome")
 	public String ricercaAutoriPerCognome(@RequestParam("cognome") String cognome, Model model){
@@ -126,24 +129,12 @@ public class GalleriaController  {
 
 
 
-	@RequestMapping(value="/galleriaAutore", method=RequestMethod.GET)
-	public String dettagliAutore(@RequestParam("id") Long id ,Model model){
-		model.addAttribute("autore", autoreservice.findbyId(id));
-		return "resultAutore";
-	}
 
 	@GetMapping("/galleriaquadri")
 	public String mostraQuadri(Model model) {
 		model.addAttribute("quadri",quadroservice.findAll());
 		return "MostraQuadri";
 	}
-
-	@RequestMapping(value="/galleriaQuadro", method=RequestMethod.GET)
-	public String dettagliQuadro(@RequestParam("id") Long id ,Model model){
-		model.addAttribute("quadro", quadroservice.findbyId(id));
-		return "resultQuadro";
-	}
-
 
 
 }
