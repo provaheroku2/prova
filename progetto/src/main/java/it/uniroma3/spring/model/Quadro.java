20e5a90e211fc1ed74bdc7b93308086f3af5b493
Nicolas Zaccaria <nic.zac95@gmail.com>
2017-06-18 22:00:54 +0200

    possibilità di aggiungere immagine al quadro

--- progetto/src/main/java/it/uniroma3/spring/model/Quadro.java
+++ progetto/src/main/java/it/uniroma3/spring/model/Quadro.java
@@ -30,18 +30,21 @@ public class Quadro {
 	@NotNull
 	@Min(0)
 	private Double altezza;
+	
+	private String immagine;
 	@ManyToOne
 	private Autore autore;
 	
 	protected Quadro(){
 	}
 	
-	public Quadro(String titolo, Integer annoRealizzazione, String tecnica, Double lunghezza, Double altezza){
+	public Quadro(String titolo, Integer annoRealizzazione, String tecnica, Double lunghezza, Double altezza, String immagine){
 		this.titolo = titolo;
 		this.annoRealizzazione = annoRealizzazione;
 		this.tecnica = tecnica;
 		this.lunghezza = lunghezza;
 		this.altezza = altezza;
+		this.immagine = immagine;
 	}
 	
 	public Long getId() {
@@ -85,5 +88,12 @@ public class Quadro {
 	}
 	public void setAutore(Autore autore) {
 		this.autore = autore;
+	}
+	
+	public String getImmagine(){
+		return this.immagine;
+	}
+	public void setImmagine(String immagine){
+		this.immagine = immagine;
 	}
 }
