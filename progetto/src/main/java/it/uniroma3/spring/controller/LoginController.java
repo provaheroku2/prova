f61675d919661f3d6601cfb9c9782dc04a19be4f
Nicolas Zaccaria <nic.zac95@gmail.com>
2017-06-19 13:01:15 +0200

    import package per logout

--- progetto/src/main/java/it/uniroma3/spring/controller/LoginController.java
+++ progetto/src/main/java/it/uniroma3/spring/controller/LoginController.java
@@ -1,8 +1,15 @@
 package it.uniroma3.spring.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // controller to access the login page
 @Controller
